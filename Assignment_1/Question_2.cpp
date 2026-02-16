ed72baca1a81b43111d27604b27466d658a8eb76
dewangdhakad <dewangdhakad8@gmail.com>
2026-02-16 23:42:54 +0530

    Question 2

---
+++ Assignment_1/Question_2.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int far,cal;
+    cout<<"Enter the temp in Celsius: ";
+    cin>>cal;
+    far=9*cal/5+32;
+    cout<<"Temp in far is "<<far;
+    return 0;
+}
